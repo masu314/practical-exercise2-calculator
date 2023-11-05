ff541e83fbc88ed1b6dc4a935e92ae9c373d91eb
EC2 Default User <ec2-user@ip-172-31-44-69.ap-southeast-2.compute.internal>
2023-11-05 15:39:49 +0000

    実践課題2（電卓）の提出-末尾から二番目が演算子の場合の処理を追加-fourthcommit

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ let result = document.getElementById("result");
 let is_calculated = false;
 //小数点が含まれているかどうか
 let included_decimal_point = false;
+//配列に演算子を代入
+let operator = ["+","-","*","/"];
 
 //ACキー押下
 function ac_click(){
@@ -12,28 +14,35 @@ function ac_click(){
   included_decimal_point = false; //小数点が含まれていない状態にする
 }
 
-//数字キー押下
+//数字キー(1~9)押下
 function num_click(num){
   //計算済みの場合
   if(is_calculated){
     result.value = "0"; //フォームの値を0にする
     is_calculated = false; //未計算の状態にする
   }
-  //フォームの値が0の状態で、0キーもしくは00キーを押した場合
-  if(result.value == "0" && (num == "0" || num == "00")){
-    result.value ="0"; //フォームの表示を0にする
+  
   //フォームの値が0の場合
-  }else if(result.value == "0"){
-    result.value = num; //フォームの表示を押した数字キーの値にする
+  if(result.value == "0"){
+    //0キーもしくは00キーを押した場合
+    if(num == "0" || num == "00"){
+      result.value = "0"; //フォーム値を0にする
+    //それ以外の場合
+    }else{
+      result.value = num; //フォーム値を押した数字キーの値にする
+    }
   //フォームの末尾が演算子の状態で、00キーを押した場合
   }else if(check_ope_last() && num == "00"){
     //値に00を追加しない
-  //フォームの末尾が0かつ小数点を含んでいない状態で、00キーを押した場合
-  }else if(result.value.slice(-1) == "0"  && !included_decimal_point && num == "00"){
-    //値に00を追加しない
-  //フォームの末尾が0かつ小数点を含んでいない状態で、00以外の数字キーを押した場合
-  }else if(result.value.slice(-1) == "0"  && !included_decimal_point){
-    result.value = result.value.slice(0,-1) + num;//フォームの末尾を削除して、押した数字キーの値を追加
+  //フォーム値の後ろから二番目が演算子で、末尾が0で、小数点を含んでいない場合
+  }else if(chechk_ope_second_last() && result.value.slice(-1) == "0" && !included_decimal_point){
+    //0もしくは00キーを押した場合
+    if(num == "0" || num == "00"){
+      //値に00もしくは0を追加しない
+    //それ以外の場合
+    }else{
+      result.value = result.value.slice(0,-1) + num; //フォームの末尾を削除して、押した演算子キーの値を追加
+    }
   //それ以外の場合
   }else{
     result.value += num; //フォームの値に、押した数字キーの値を追加
@@ -56,8 +65,8 @@ function point_click(point){
     //小数点を追加しない
   //それ以外の場合
   }else{
-  result.value += point; //フォームの値に、小数点を追加
-  included_decimal_point = true; //小数点が含まれている状態にする
+    result.value += point; //フォームの値に、小数点を追加
+    included_decimal_point = true; //小数点が含まれている状態にする
   }
 }
 
@@ -98,12 +107,16 @@ function equal_click(){
     result.value = "ERROR"; //フォームの表示をERRORにする
   //それ以外の場合
   }else{
-  result.value = calculation_result; //フォームに計算結果を表示
+    result.value = calculation_result; //フォームに計算結果を表示
   }
 }
 
 //フォームの末尾が演算子かどうか
 function check_ope_last(){
-  let operator = ["+","-","*","/"]; //配列に演算子を代入
   return operator.includes(result.value.slice(-1)); //演算子にフォームの末尾が含まれているかどうかチェックして、その結果（真偽値）を戻り値とする
+}
+
+//フォーム値の後ろから二番目が演算子かどうか
+function chechk_ope_second_last(){
+  return operator.includes(result.value.slice(-2,-1)); //演算子にフォームの後ろから二番目の値が含まれているかどうかチェックして、その結果を戻り値とする
 }
